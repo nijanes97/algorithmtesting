d047dd3d4462f33fbd3324cd9b9ad39f5bcae598
nijanes97 <nickjanes97@gmail.com>
2020-08-26 22:19:28 -0700

    more algos

--- division-without-division/division.py
+++ division-without-division/division.py
@@ -23,3 +23,33 @@ class Solution:
                 answer |= 1 << i
                 
         return sign * answer
+        
+from collections import Counter
+def frequencyQueries(queries):
+    # Write your code here
+    # keeps a tally of which numbers have been inserted
+    numCounter = Counter()
+    # keeps a tally of which frequencies are currently present in the collection
+    frequencyCounter = Counter()
+    queryReturns = []
+
+    for query, value in queries:
+        if query == 1:
+            # stores the number being added to numCounter, then adds 1 to the frequency counter for that frequency
+            frequencyCounter[numCounter[value]] -= 1
+            numCounter[value] += 1
+            frequencyCounter[numCounter[value]] += 1
+        elif query == 2:
+            if numCounter[value] > 0:
+                # updates the stored numbers and the frequencies if the number exists in the collection
+                frequencyCounter[numCounter[value]] -= 1
+                numCounter[value] -= 1
+                frequencyCounter[numCounter[value]] += 1   
+        elif query == 3:
+            # adds 1 or 0 to the return arr if the frequency exists
+            if frequencyCounter[value] > 0:
+                queryReturns.append(1)
+            else:
+                queryReturns.append(0)
+
+    return queryReturns
